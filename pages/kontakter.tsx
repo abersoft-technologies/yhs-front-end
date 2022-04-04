194c7f20fc79a9283f2614952593e547d9a06059
JesperKYH <jesper.pettersson@student.kyh.se>
2022-04-04 15:27:25 +0200

    YS-52 Fix dropdown logic

--- pages/kontakter.tsx
+++ pages/kontakter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Ref, useRef, useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
@@ -16,13 +16,25 @@ import AddCorporateModule from '../src/components/modules/add_data/AddCorporateM
 
 const kontakter: NextPage = () => {
   const router = useRouter();
-  const [contactModuleToggle, setContactModuleToggle] =
-    useState<boolean>(false);
-    const [corpModuleToggle, setCorpModuleToggle] =
-    useState<boolean>(false);
+  const toggleRef = React.createRef<HTMLButtonElement>();
+  const [contactModuleToggle, setContactModuleToggle] = useState<boolean>(false);
+  const [corpModuleToggle, setCorpModuleToggle] = useState<boolean>(false);
+  const [toggleButtons, setToggleButtons] = useState<boolean>(false);
+  const [topPos, setTopPos] = useState<number>(0);
+
+
 
   const closeContactModule = () => setContactModuleToggle(false);
   const closeCorpModule = () => setCorpModuleToggle(false);
+
+  const calculateDropdown = () => {
+    if(toggleRef.current) {
+      const rect = toggleRef.current.getBoundingClientRect();
+      const tempTopPos = rect.bottom.toFixed(0);
+      const number = parseInt(tempTopPos)
+      setTopPos(number + 10)
+    }
+  }
 
 
   return (
@@ -55,8 +67,25 @@ const kontakter: NextPage = () => {
               <img src='/filter-icon.svg' alt='Filter icon' /> Filter
             </button>
           </div>
-
-          <Flex direction='row' gap='large'>
+          <Flex direction='column' gap='large' align='center' justify='center'>
+          <Button
+              iconRight={<img src='/arrow-toggle-down.svg' alt='toggle' />}
+              size='xs'
+              onClick={() => {
+                setToggleButtons(!toggleButtons)
+                calculateDropdown()
+              }}
+              auto
+              ref={toggleRef}
+              css={{
+                background: '#7586ce',
+                fontSize: '0.75rem',
+                fontWeight: '400',
+            }}>
+            Lägg till
+          </Button>
+          {toggleButtons ?
+          <div className={styles.buttons_container} style={{top: topPos}}>
             <Button
               onClick={() => setContactModuleToggle(!contactModuleToggle)}
               auto
@@ -83,6 +112,8 @@ const kontakter: NextPage = () => {
             >
               Företag
             </Button>
+          </div> : <></>
+          }
           </Flex>
         </header>
         <ContactList />
@@ -92,8 +123,8 @@ const kontakter: NextPage = () => {
         closeModule={closeContactModule}
       />
       <AddCorporateModule
-      active={corpModuleToggle}
-      closeModule={closeCorpModule}/>
+        active={corpModuleToggle}
+        closeModule={closeCorpModule}/>
     </>
   );
 };
