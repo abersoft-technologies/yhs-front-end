ba804e756645ebd7d4b0bf246554ea4226bba44d
Jesper Pettersson <70097615+Jesper-dev@users.noreply.github.com>
2022-04-07 15:22:12 +0200

    YS-68 Add education from client (#24)

---
+++ src/apis/edu/add.ts
@@ -0,0 +1,20 @@
+import api from '../api';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+
+interface IAddCorpParams {
+    name: string,
+    place: string[],
+}
+
+export const addEdu = async (data: IAddCorpParams) => {
+  const userData = useLocalStorage('get', 'session', 'user');
+  const token = userData.data.token;
+  try {
+    const result = await api.post(`/edu/add`, data, {
+      headers: { 'x-access-token': token },
+    });
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
